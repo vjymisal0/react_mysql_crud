e0a5cfa1cd9e058e8d098791958ccd36601c5471
Vijay Misal <misalvijay153@gmail.com>
2024-02-11 11:47:19 +0530

    inserting data in mysql through backend

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import mysql from 'mysql';
+const app = express()
+const port = 3000
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: "react_crud"
+    }
+)
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome to our application" })
+})
+app.get('/books', (req, res) => {
+    const sql = "SELECT * FROM crud_react"
+    db.query(sql, (err, result) => {
+        if (err) throw err
+        res.json(result)
+    })
+})
+
+app.post("/books", (req, res) => {
+
+    const sql1 = "INSERT INTO crud_react (`title`, `descr`, `cover`) VALUES(?)"
+
+    const VALUES = [
+        "title from backend",
+        "description from backend",
+        "cover from backend",]
+    db.query(sql1, [VALUES], (err, result) => {
+        if (err) return res.json({ message: err })
+        return res.json("Book has been created successfully")
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+}
+)
